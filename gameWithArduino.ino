17d87ffd132a1dacad0d70347f0b638cfebddaaa
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-11-25 16:26:14 -0600

    Add files via upload

--- gameWithArduino.ino
+++ gameWithArduino.ino
@@ -2,8 +2,11 @@
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+unsigned long tiempoAnterior = 0;
 class protagonist {
   public:
+    char direct = "derecha";
+    bool shoot = false;
     int x = 0, y = 0;
     byte body[8] = {
       B11100,
@@ -20,47 +23,55 @@ class protagonist {
     void dmove(LiquidCrystal_I2C lcd ) {
       String input = Serial.readString();
       char m = input.charAt(0);
-      Serial.print(" el tu estas en las coordenadas x,y ");
-      Serial.println(x, y);
       switch (m) {
         case 'w':
           if (y > 0) {
             y--;
-         
+            direct = "arriba";
+
           }
           break;
         case 's':
           if (y < 4) {
             y++;
+            direct = "abajo";
 
           }
           break;
         case 'd':
           if (x < 20) {
             x++;
-        
+            direct = "derecha";
+
+
           }
           break;
         case 'a':
           if (x > 0) {
             x--;
-      
+            direct = "izquierda";
+
           }
 
+          break;
+        case 'x':
+          shoot = true;
           break;
         default:
 
           break;
       }
       lcd.setCursor(x, y);
-      lcd.write(byte(body));
-
+      lcd.write("y");
     }
+
 };
 
 class enemy {
 
   public:
+    bool die = false;
+    int x , y ;
 
     byte body[8] = {
       B11111,
@@ -73,34 +84,70 @@ class enemy {
       B00000
     };
 
-    int x , y ;
+
     void set() {
       x = random(5, 19);
       y = random(4);
+
     }
-    void mover( LiquidCrystal_I2C lcd, int xP, int yP) {
-      int t = millis();
-      if (t % 1500) {
-        // move to a point
-        if ( x < xP) {
-          
-          x++;
+    void mover( LiquidCrystal_I2C lcd, int xP, int yP, bool shoot, char direct) {
+
+
+      // move to a point
+      if ( x < xP) {
+
+        x++;
+      }
+      else if (x > xP) {
+        x--;
+      }
+      else {
+        if (y < yP) {
+          y++;
         }
-        else if (x > xP) {
-          x--;
+        else if (y > yP) {
+          y--;
         }
-        else {
-          if (y < yP) {
-            y++;
-          }
-          else if (y > yP) {
-            y--;
-          }
+      }
+      if (shoot) {
+        switch (direct) {
+          case 'izquierda':
+            die = y == yP;
+            Serial.print(direct);
+            Serial.print(xP, yP);
+            Serial.println(x, y );
+
+            break;
+          case 'derecha':
+            die = y == yP;
+            Serial.print(direct);
+            Serial.print(xP, yP);
+            Serial.println(x, y );
+            break;
+          case 'abajo':
+            die = x == xP;
+            Serial.print(direct);
+            Serial.print(xP, yP);
+            Serial.println(x, y );
+            break;
+          case 'arriba':
+            die = x == xP;
+            Serial.print(direct);
+            Serial.print(xP, yP);
+            Serial.println(x, y );
+            break;
+          default:
+            Serial.print(direct);
+            Serial.print(xP, yP);
+            Serial.println(x, y );
+            break;
         }
       }
       // move to a point
+
       lcd.setCursor(x, y);
-      lcd.write(body);
+      lcd.print("e");
+
     }
 };
 char m[] = "helloWorld";
@@ -115,14 +162,25 @@ void setup() {
 
 }
 void loop() {
-  int t = millis();
-  pro.dmove(lcd);
+  bool h = ene.x == pro.x && ene.y == pro.y;
 
-  ene.mover(lcd, pro.x, pro.y);
-  if (ene.x == pro.x && ene.y == pro.y) {
-    Serial.println("haz perdido");
+  if (h) {
+    lcd.clear();
+    lcd.setCursor(10 - (sizeof("you lost") / 2), 2);
+    lcd.print("you lost");
+    delay(100);
+    lcd.clear();
+
   }
-  if ( t % 1000) {
+  else if (ene.die) {
+    lcd.setCursor(10 - (sizeof("YOU WIN") / 2), 2);
+    lcd.print("YOU WIN");
+  }
+  else {
+    pro.dmove(lcd);
+    delay(500);
+    ene.mover(lcd, pro.x, pro.y, pro.shoot, pro.direct);
+    delay(1000);
     lcd.clear();
   }
 }
